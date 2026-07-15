d677a31c490bf2a1af67a1a74a556b9b02fa9f10
grey-inheritance <mcrseilles@gmail.com>
2026-07-15 15:41:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+document.body.addEventListener("click", (ev) => {
+  const isExpTitle = !!ev.target.closest(".spoiler__title-bar");
+  const spoiler = ev.target.closest(".spoiler");
 
-$(function() {
-  $('.spoilerrrrr').click(function() {
-    $(this).find('p').slideToggle();
-  });
+  if (!isExpTitle) {
+    return;
+  }
+
+  spoiler.classList.toggle("spoiler--open")
 });
