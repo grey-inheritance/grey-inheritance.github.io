1d5da06edec5203d08a6226ce2b77e8960a594b0
grey-inheritance <mcrseilles@gmail.com>
2026-07-17 15:41:18 -0400

    Update and rename index.js to code/index.js

---
+++ code/index.js
@@ -0,0 +1,18 @@
+document.body.addEventListener("click", (ev) => {
+  const isExpTitle = !!ev.target.closest(".spoiler__title-bar");
+  const spoiler = ev.target.closest(".spoiler");
+
+  if (!isExpTitle) {
+    return;
+  }
+
+  spoiler.classList.toggle("spoiler--open")
+});
+
+// Fetch the "your-text-file.txt" file and inject it into the "text-container" div
+fetch('your-text-file.txt')
+  .then(response => response.text())
+  .then(data => {
+      document.getElementById('text-container').innerText = data;
+  })
+  .catch(error => console.error('Error loading file:', error));
